e7ee8999dab682696f8116f781de119f990e1b9d
Diyar <diyar.ildart@gmail.com>
2023-05-25 19:48:43 +0600

    feature: AI service for checking for toxic text

---
+++ serviceAI/backend/main.py
@@ -0,0 +1,23 @@
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.ensemble import RandomForestClassifier
+
+from flask import Flask, request, jsonify
+
+# Load the vectorizer and model
+model = pickle.load(open('random_forest_model.pkl', 'rb'))
+vectorizer = pickle.load(open('vectorizer.pkl', 'rb'))
+app = Flask(__name__)
+
+@app.route("/evaluate_text", methods=["POST"])
+def process_text():
+    data = request.get_json()
+    text = data.get("text")
+    input_vector = vectorizer.transform([text])
+    pred = model.predict(input_vector)
+    print(pred)
+
+    return jsonify(pred.tolist())
+
+if __name__ == "__main__":
+    app.run()
